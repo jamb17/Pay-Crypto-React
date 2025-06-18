1a54f839dd92d7e1a4e5c7b227747c14aa25034d
jamb17 <dimanesterov11@gmail.com>
2025-06-18 08:09:32 +0300

    Redesigned for a serverless approach to deploy on Vercel

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 import express from "express";
 import mongoose from "mongoose";
-import UserRouter from "./routes/userRoute.js";
+import UserRouter from "../routes/userRoute.js";
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import ServerlessHttp from 'serverless-http';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors({
@@ -18,13 +18,11 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use('/user', UserRouter);
 
-async function startApp() {
-    try {
-        await mongoose.connect(process.env.DB_URL);
-        app.listen(PORT, console.log('Succeed - ' + PORT));
-    } catch(e) {
-        console.log(e);
-    }
-}
+const handler = ServerlessHttp(app)
 
-startApp();
+export default async (req, res) => {
+    if (mongoose.connection.readyState !== 1) {
+        await mongoose.connect(process.env.DB_URL)
+    }
+    return handler(req, res)
+}
