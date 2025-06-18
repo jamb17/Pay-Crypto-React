14c20ec3dacfa434a47bc3cc27f478de85b1d454
jamb17 <dimanesterov11@gmail.com>
2025-06-18 10:22:50 +0300

    fixed CORS

--- backend/api/server.js
+++ backend/api/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://paycrypto-zeta.vercel.app/',
+    origin: 'https://paycrypto-zeta.vercel.app',
     credentials: true
 }));
 app.use(cookieParser());
