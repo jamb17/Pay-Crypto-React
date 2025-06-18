d6d5eeb84f4fd4470787bb103fce3f054f8a7ca5
jamb17 <dimanesterov11@gmail.com>
2025-06-18 11:08:24 +0300

    fixed cors options

--- backend/api/server.js
+++ backend/api/server.js
@@ -18,7 +18,7 @@ const corsOptions = {
 
 app.use(express.json());
 app.use(cors(corsOptions));
-app.options('*', corsOptions);
+app.options('*', cors(corsOptions));
 app.use(cookieParser());
 app.use(express.urlencoded({
     extended: true
