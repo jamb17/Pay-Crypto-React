d42a76a10cc350ea588bf3bcdfbfc008b70bd6ac
jamb17 <dimanesterov11@gmail.com>
2025-06-26 13:59:21 +0300

    Fixed server and gitignore

--- backend/api/server.js
+++ backend/api/server.js
@@ -19,7 +19,7 @@ const corsOptions = {
 
 app.use(express.json());
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 app.use(cookieParser());
 app.use(express.urlencoded({
     extended: true
