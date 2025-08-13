de9fc044e8a56a1a1f3faa11a6a6296185e2f385
jamb17 <dimanesterov11@gmail.com>
2025-08-13 15:30:05 +0300

    added display of all accounts of both types at once

--- frontend/src/Features/PersonalAccount/components/ActionSection.jsx
+++ frontend/src/Features/PersonalAccount/components/ActionSection.jsx
@@ -7,46 +7,65 @@ import donateIcon from '@assets/donate-icon.svg'
 import bgVector from '@assets/bg-vector.svg'
 import bgVectorLight from '@assets/bg-vector-light.svg'
 import openedMerchantIcon from '@assets/openedMerchantIcon.svg'
+import openedDonateIcon from '@assets/openedDonateIcon.svg'
 
-export default function ActionSection({ type, setOpenPopUp, merchant }) {
+export default function ActionSection({ type, setOpenPopUp, setPopUpType, merchant = [], donate = [] }) {
     const { theme } = useContext(ThemeContext);    const containerRef = useRef(null);
-    useGsapSlideUp(containerRef, { scale: 1 }, { duration: .6 });
+    useGsapSlideUp(containerRef, { scale: 1 }, { duration: .6, delay: .3});
 
-    const defaultSection = <div ref={containerRef} style={theme ? { borderTop: 'none' } : {}} className={type === 'merchant' ? styles.container : styles.containerDonate}>
+    const handleOpenPopUp = (popUpType) => {
+        setOpenPopUp(true)
+        setPopUpType(popUpType)
+    }
+
+    const defaultSection = <div ref={containerRef} style={theme ? { borderTop: 'none' } : {}} className={type.includes('merchant') ? styles.container : styles.containerDonate}>
         <div className={styles.content}>
             <div className={styles.text}>
-                <h2>Let’s create your first {type === 'merchant' ? 'merchant' : 'donate'}!</h2>
+                <h2>Let’s create your first {type.includes('merchant') ? 'merchant' : 'donate'}!</h2>
                 <p>Time to accept payments!</p>
             </div>
-            <button onClick={() => setOpenPopUp(true)} className={styles.createBtn}>
+            <button onClick={() => {type.includes('merchant') ? handleOpenPopUp('merchant') : handleOpenPopUp('donate')}} className={styles.createBtn}>
                 <div className={styles.plusIcon} />
                 Create
             </button>
         </div>
-        <img className={styles.illustration} src={type === 'merchant' ? merchantIcon : donateIcon} />
-        <img className={styles.bgVector} src={type === 'merchant' ? bgVectorLight : bgVector} />
+        <img className={styles.illustration} src={type.includes('merchant') ? merchantIcon : donateIcon} />
+        <img className={styles.bgVector} src={type.includes('merchant') ? bgVectorLight : bgVector} />
     </div>
 
-    const openedSection = <div className={styles.containerOpened}>
-        <div className={styles.content}>
-            <div className={styles.text}>
-                <h2>Your merchant accounts</h2>
-                <p>Time to accept payments!</p>
-            </div>
-            <img src={openedMerchantIcon} />
-        </div>
-        <ul className={styles.accountsList}>
-            <li className={styles.account}>
-                <img src={merchant?.file} className={styles.avatar} />
+    const merchantList = merchant.map((merchant, index) => {
+        return (<li className={styles.account} key={index}>
+                {merchant.file !== '' ? <img src={merchant?.file} className={styles.avatar} /> : <div className={styles.avatar}>{merchant?.name[0]}</div>}
                 <div className={styles.info}>
                     <p>{merchant?.name}</p>
                 </div>
-            </li>
+            </li>)
+    })
+
+    const donateList = donate.map((e, index) => {
+        return (<li className={styles.account} key={index}>
+                {e.file !== '' ? <img src={e?.file} className={styles.avatar} /> : <div className={styles.avatar}>{e?.name[0]}</div>}
+                <div className={styles.info}>
+                    <p>{e?.name}</p>
+                </div>
+            </li>)
+    })
+
+    const openedSection = <div ref={containerRef} className={type.includes('merchant') ? styles.containerOpened : styles.containerDonateOpened}>
+        <div className={styles.content}>
+            <div className={styles.text}>
+                <h2>Your {type.includes('merchant') ? "merchant" : "donate"} accounts</h2>
+                <p>Time to accept payments!</p>
+            </div>
+            <img src={type.includes('merchant') ? openedMerchantIcon : openedDonateIcon} />
+        </div>
+        <ul className={styles.accountsList}>
+            {type.includes('merchant') ? merchantList : donateList}
         </ul>
         <div className={styles.btnContainer}>
-                <button onClick={() => setOpenPopUp(true)} className={styles.createBtn}>
-                    {/* <div className={styles.plusIcon} /> */}
-                    Create new merchant account
+                <button onClick={() => {type.includes('merchant') ? handleOpenPopUp('merchant') : handleOpenPopUp('donate')}} className={styles.createBtn}>
+                    <div className={styles.plusIcon} />
+                    Create new {type.includes('merchant') ? 'merchant' : 'donate'} account
                 </button>
             </div>
     </div>
