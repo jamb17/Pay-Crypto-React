314148d24c456b20e52a4ea111ed88f3017dada5
jamb17 <dimanesterov11@gmail.com>
2025-08-13 15:40:22 +0300

    removed theme switcher until it'll be revorked

--- frontend/src/ThemeContext.jsx
+++ frontend/src/ThemeContext.jsx
@@ -4,14 +4,14 @@ export const ThemeContext = createContext();
 
 export const ThemeProvider = ({children}) => {
     const [theme, setTheme] = useState(false);
-    useLayoutEffect(() => {
-        if (window.localStorage.getItem('theme') === 'dark') {
-            setTheme(true);
-        } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && !window.localStorage.getItem('theme')) {
-            setTheme(true);
-            window.localStorage.setItem('theme', 'dark');
-        }
-    }, []);   
+    // useLayoutEffect(() => {
+    //     if (window.localStorage.getItem('theme') === 'dark') {
+    //         setTheme(true);
+    //     } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && !window.localStorage.getItem('theme')) {
+    //         setTheme(true);
+    //         window.localStorage.setItem('theme', 'dark');
+    //     }
+    // }, []);   
 
     return (
         <ThemeContext.Provider value={{theme, setTheme}}>
