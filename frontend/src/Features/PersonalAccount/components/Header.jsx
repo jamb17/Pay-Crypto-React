a7e4d0f2bfcfcd9550a64db10b57f27bb7ba5345
jamb17 <dimanesterov11@gmail.com>
2025-08-22 20:46:30 +0300

    dropdown menu added (with logout button and theme switcher)

--- frontend/src/Features/PersonalAccount/components/Header.jsx
+++ frontend/src/Features/PersonalAccount/components/Header.jsx
@@ -4,10 +4,24 @@ import Navigation from "./Navigation";
 import imagePlaceholder from '@assets/image-placeholder.png'
 import { Link } from "react-router-dom";
 import { useShallow } from "zustand/react/shallow";
+import ThemeSwitcher from '@components/ThemeSwitcher.jsx'
+import { useRef, useState } from "react";
+import useGsapSlideDown from '@hooks/useGsapSlideDown.js'
 
 function Header() {
 
     const nickname = useStore(useShallow(state => state.nickname));
+
+    const [dropDownOpened, setDropDownOpened] = useState(false)
+
+    const dropdownMenuRef = useRef(null)
+    useGsapSlideDown(dropdownMenuRef, {y: -40, scale: 1}, {duration: .5}, dropDownOpened)
+
+    const handleClick = () => {
+        setDropDownOpened(prev => !prev)
+    }
+
+    const logout = useStore(state => state.logout)
 
     return (
         <>
@@ -24,6 +38,16 @@ function Header() {
                                 src={imagePlaceholder}
                                 className={styles.avatar}
                             />
+                            <div onClick={handleClick} style={dropDownOpened ? {transform: 'rotate(180deg)'} : {}} className={styles.dropdownBtn}></div>
+                            {dropDownOpened && <>
+                                <div className={styles.dropdownMenu} ref={dropdownMenuRef}>
+                                    <div className={styles.themeSwitcherContainer}>
+                                        <p>Theme:</p>
+                                        <ThemeSwitcher />
+                                    </div>
+                                    <button onClick={logout} className={styles.logout}>Logout</button>
+                                </div>
+                            </>}
                         </div>
                     </div>
                 </div>
