9d67b470b2ac8b6df25935a3657a2e7993a47acb
jamb17 <dimanesterov11@gmail.com>
2025-08-28 23:47:17 +0300

    dev setup added

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,15 +1,14 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv/config';
 import express from "express";
 import mongoose from "mongoose";
 import UserRouter from "../routes/userRoute.js";
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-dotenv.config();
 const app = express();
 
 const corsOptions = {
-    origin: 'https://paycrypto-zeta.vercel.app',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
@@ -27,17 +26,28 @@ app.get('/', (req, res) => {
     res.send("Works");
 })
 
-export default async function handler(req, res) {
-  if (mongoose.connection.readyState !== 1) {
-    console.log('🗄️  Connecting to MongoDB…');
-    try {
-      await mongoose.connect(process.env.DB_URL);
-      console.log('✅ MongoDB connected');
-    } catch (err) {
-      console.error('❌ MongoDB connection error:', err);
-      return res.status(500).send('DB connection failed');
-    }
-  }
+// export default async function handler(req, res) {
+//   if (mongoose.connection.readyState !== 1) {
+//     console.log('🗄️  Connecting to MongoDB…');
+//     try {
+//       await mongoose.connect(process.env.DB_URL);
+//       console.log('✅ MongoDB connected');
+//     } catch (err) {
+//       console.error('❌ MongoDB connection error:', err);
+//       return res.status(500).send('DB connection failed');
+//     }
+//   }
 
-  return app(req, res);
+//   return app(req, res);
+// }
+
+async function startApp() {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        app.listen(process.env.PORT, console.log('Succeed - ' + process.env.PORT));
+    } catch(e) {
+        console.log(e);
+    }
 }
+
+startApp();
