3fe593fd09d1f6a63179b4b8a1d24c6aa3189ff0
jamb17 <dimanesterov11@gmail.com>
2025-09-10 14:53:19 +0300

    layout structure changed

--- frontend/src/Features/PersonalAccount/components/Settings.jsx
+++ frontend/src/Features/PersonalAccount/components/Settings.jsx
@@ -1,14 +1,20 @@
 import Header from './Header.jsx'
 import { ChangePasswordForm } from './ChangePasswordForm.jsx'
 import { ChangeAvatar } from './ChangeAvatar.jsx'
+import { ChangeNickname } from './ChangeNickname.jsx'
 
 export const Settings = () => {
 
     return <>
         <Header />
         <div className='grid gap-5 w-full justify-center grid-cols-[repeat(2,_456px)] justify-items-center'>
-            <ChangePasswordForm />
-            <ChangeAvatar />
+            <div className='flex flex-col gap-5 w-full'>
+                <ChangePasswordForm />
+                <ChangeNickname/>
+            </div>
+            <div className='flex flex-col gap-5 w-full'>
+                <ChangeAvatar />
+            </div>
         </div>
     </>
 }
