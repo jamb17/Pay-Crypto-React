cd8cbf8212d7585c4a9e7290179a326eebd25591
jamb17 <dimanesterov11@gmail.com>
2025-09-20 15:29:42 +0300

    gradient bg in light theme added

--- frontend/src/reactbits/GlitchText/GlitchText.jsx
+++ frontend/src/reactbits/GlitchText/GlitchText.jsx
@@ -9,10 +9,10 @@ const GlitchText = ({ children, speed = 0.5, enableShadows = true, enableOnHover
   const baseClasses = 'text-[var(--text-color)] text-[clamp(2rem,10vw,8rem)] font-black relative mx-auto select-none cursor-pointer text-center';
 
   const pseudoClasses = !enableOnHover
-    ? 'after:content-[attr(data-text)] after:absolute after:top-0 after:left-[10px] after:text-[var(--text-color)] after:bg-[var(--bg-color)] after:overflow-hidden after:[clip-path:inset(0_0_0_0)] after:[text-shadow:var(--after-shadow)] after:animate-glitch-after ' +
-      'before:content-[attr(data-text)] before:absolute before:top-0 before:left-[-10px] before:text-[var(--text-color)] before:bg-[var(--bg-color)] before:overflow-hidden before:[clip-path:inset(0_0_0_0)] before:[text-shadow:var(--before-shadow)] before:animate-glitch-before'
-    : "after:content-[''] after:absolute after:top-0 after:left-[10px] after:text-[var(--text-color)] after:bg-[var(--bg-color)] after:overflow-hidden after:[clip-path:inset(0_0_0_0)] after:opacity-0 " +
-      "before:content-[''] before:absolute before:top-0 before:left-[-10px] before:text-[var(--text-color)] before:bg-[var(--bg-color)] before:overflow-hidden before:[clip-path:inset(0_0_0_0)] before:opacity-0 " +
+    ? 'after:content-[attr(data-text)] after:absolute after:top-0 after:left-[10px] after:text-[var(--text-color)] after:bg-[transparent] after:overflow-hidden after:[clip-path:inset(0_0_0_0)] after:[text-shadow:var(--after-shadow)] after:animate-glitch-after ' +
+      'before:content-[attr(data-text)] before:absolute before:top-0 before:left-[-10px] before:text-[var(--text-color)] before:bg-[transparent] before:overflow-hidden before:[clip-path:inset(0_0_0_0)] before:[text-shadow:var(--before-shadow)] before:animate-glitch-before'
+    : "after:content-[''] after:absolute after:top-0 after:left-[10px] after:text-[var(--text-color)] after:bg-[transparent] after:overflow-hidden after:[clip-path:inset(0_0_0_0)] after:opacity-0 " +
+      "before:content-[''] before:absolute before:top-0 before:left-[-10px] before:text-[var(--text-color)] before:bg-[transparent] before:overflow-hidden before:[clip-path:inset(0_0_0_0)] before:opacity-0 " +
       'hover:after:content-[attr(data-text)] hover:after:opacity-100 hover:after:[text-shadow:var(--after-shadow)] hover:after:animate-glitch-after ' +
       'hover:before:content-[attr(data-text)] hover:before:opacity-100 hover:before:[text-shadow:var(--before-shadow)] hover:before:animate-glitch-before';
 
